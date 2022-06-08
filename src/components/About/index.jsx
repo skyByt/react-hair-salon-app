4698035021e77d3201db90adddbcaa2c6ef0153e
skyByt <fredjkharroubi@gmail.com>
2022-06-08 01:03:39 -0400

    update header and navbar

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,22 +5,19 @@ import img from '../Assets/about-img.jpg'
 
 const About = () => {
   return (
-      <div className='container-fluid'>
-        <div className='row'>
-          <section className='col-xs-12 col-sm-12 col-md-6'>
-              <img src={img} className='img-fluid' alt='barbershop' width='600'/>
-          </section>
-          <section className='text-section col-xs-12 col-sm-12 col-md-6'>
-            <h3>about</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-              commodo consequat.
-            </p>
-          </section>
-        </div>
-
+    <div className='wrapper row'>
+      <div className='img col-xs-12 col-sm-12 col-md-6'>
+      <img src={img} className='img' alt='barbershop'/>
       </div>
+      <div className='info col-xs-12 col-sm-12 col-md-6'>
+        <h3>about</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+          commodo consequat.
+        </p>
+      </div>
+    </div>
   )
 }
 
